7d5d418ed079d2236ad0e2517d018ba9bcc83e3b
ValeriyKulinko <valeriykulinko@gmail.com>
2023-02-16 16:49:25 +0200

    jjkj

--- src/Copmponents/Dialogs/Dialogs.jsx
+++ src/Copmponents/Dialogs/Dialogs.jsx
@@ -8,7 +8,6 @@ const Dialogs = (props) => {
 
     let dialogsElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
     let messagesElements = props.state.messages.map(m => <Message message={m.message} />);
-<<<<<<< HEAD
 
     let newMessageElement = React.createRef();
 
@@ -17,8 +16,6 @@ const Dialogs = (props) => {
         alert(text);
     }
 
-=======
->>>>>>> 262004979f49da77f88d322da6ab5bcb5808f5a1
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItem}>
@@ -26,11 +23,8 @@ const Dialogs = (props) => {
             </div>
             <div className={s.messages}>
                 {messagesElements}
-<<<<<<< HEAD
                 <div><textarea ref={newMessageElement}></textarea></div>
                 <div><button onClick={addMessage}>Add Message</button></div>
-=======
->>>>>>> 262004979f49da77f88d322da6ab5bcb5808f5a1
             </div>
         </div>
     )
